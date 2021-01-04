221d3fb5f6c029036afb28adba38fb70ec378a0e
Dhaval Tandale <dhaval.tan@gmail.com>
2021-01-04 13:13:24 +0530

    first commit

---
+++ projectmanagementtool/src/test/java/com/dtan/projectmanagementtool/ProjectmanagementtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtan.projectmanagementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagementtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
